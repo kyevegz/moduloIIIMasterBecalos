8a89c83cfc04e666b2deec204ce6e1d0d5bdd98a
kyevegz <jimenacarrillocastillo@gmail.com>
2026-01-29 19:35:26 -0600

    hola

---
+++ clase29enero2026.js
@@ -0,0 +1,36 @@
+var nombre = "Jimena";
+let apellido = "Carrillo";
+console.log(nombre);
+console.log(apellido);
+
+//uso de var
+//es exactamente lo mismo que...
+
+//usi de let
+// let apellido = "bravo";
+// apellido = "Vidales";
+// let edad = 34;
+//no se puede tener el mismo nombre de variable
+
+
+//OPERADORES BÁSICOS
+
+//-Asignación:
+let asignarNombre = "mena";
+
+//-Matemáticos:
+//--Suma
+console.log(2+2);
+
+//--resta
+console.log(20-8);
+console.log(9-100);
+
+//--división
+console.log(10/2);
+
+//--módulo
+console.log(10%3);
+
+//--potencia
+console.log(2**2);
