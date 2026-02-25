a4338f201beff74134a49721ac3c71b6101b53bd
kyevegz <jimenacarrillocastillo@gmail.com>
2026-02-25 16:55:02 -0600

    formato de fecha

--- proyectoFinalComentarios/script.js
+++ proyectoFinalComentarios/script.js
@@ -1,56 +1,89 @@
-let formulario = document.getElementById(('form-comments'));
-let nombre = "", apellido = "", comentario = "", Tusuario = "", 
+let formulario = document.getElementById(('form-comments'));//obtiene el formulario
+let nombre = "", apellido = "", comentario = "", Tusuario = "", //variables para los datos del usuario y los elementos que se crearán en el DOM
 Tfecha = "", TComentario = "", Tdivisor = "", Timagen = "", TdivisorFlex = "",
 TEliminar = "";
-let fechaAct = "";
-let contador = 0;
-const section = document.getElementById('comment-list');
-let spanContador = document.getElementById("contador");
-spanContador.textContent = contador.toString();
+let fechaAc = "";//variable que guardará la fecha
+let contador = 0;//variable contadora de comentarios
+const section = document.getElementById('comment-list');//constante que accede al div donde se enlistarán los comentarios
+let spanContador = document.getElementById("contador");//variable que obtiene el span donde se muestra la cantidad de comentarios
+spanContador.textContent = contador.toString();//lo inicializa con el valor de 0
 
+
+//arreglos constantes para formato de fecha
+const diasSp = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes",
+    "Sábado" ], 
+    meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
 formulario.addEventListener('submit', function(event){
-    fechaAct = new Date().toString()
-    //creación de los elementos en el DOM
+
+    
+    //-----formato de fecha------- La fecha sale en inglés, así que para mayor estética la vamos a formatear
+    fechaAc = new Date();
+        let [mes, diaS, diaN, hora, minuto, segundo] = [
+            fechaAc.getMonth(),
+            fechaAc.getDay(),
+            fechaAc.getDate(),
+            fechaAc.getHours(),
+            fechaAc.getMinutes(),
+            fechaAc.getSeconds(),
+        ];
+console.log(mes, diaS, hora, minuto,segundo);
+    for(let i = 0; i < 7; i++){//recorre el array de días de semana hasta que el valor numérico coincida con el día en español
+        if(i === diaS ){
+            diaS = diasSp[i];
+        }
+    }
+
+    for(let i = 0; i < 12; i++){//recorre el array de los 12 meses, aquí el mes sí empieza desde 0 
+        if(i === mes){
+            mes = meses[i];
+        }
+    }
+    
+    //----------------creación de los elementos en el DOM--------------
+    console.log(mes, diaS, hora, minuto, segundo);
+    //h3 del usuario
     Tusuario = document.createElement('h3');
     Tusuario.id = 'tag-name'+contador.toString();
 
+    //h5 de la fecha
     Tfecha = document.createElement('h5');
     Tfecha.id = 'tag-date'+contador.toString();
 
+    //p del comentario
     TComentario = document.createElement('p');
     TComentario.id = 'tag-comment'+contador.toString();
 
+    //img de la foto
     Timagen = document.createElement('img');
     Timagen.src = "img/img1.png";
     Timagen.id = "imc"+contador.toString();
 
+    //div que le da el flex y pone la imágen y el p y los h en fila
     TdivisorFlex = document.createElement('div');
     TdivisorFlex.id = "divF"+contador.toString();
 
+    //button para eliminar
+    TEliminar = document.createElement('button');
+    TEliminar.id = "delete"+contador.toString();
+    TEliminar.className = "delete-comment";
+
+    //div que contiene todo lo anterior
     Tdivisor = document.createElement('div');
     Tdivisor.id = "comment"+contador.toString();
     Tdivisor.className = "comment";
 
-    TEliminar = document.createElement('button');
-    TEliminar.id = "delete"+contador.toString();
-    TEliminar.className = "delete-comment";
-    //Tdivisor.textContent = "caifanes";
-
-
-
     event.preventDefault();//evita que el form se recargue
 
-    //obtención de valores ingresados por el form
+    //-------------obtención de valores ingresados por el form---------------
     nombre = document.getElementById('input-name').value;
     apellido = document.getElementById('input-lastname').value;
     comentario = document.getElementById('input-comment').value;
-    //alert(nombre + " " + apellido + " " + comentario);
-    
     
 
-    //asignación del contenido que llevarán las etiquetas ya creadas
+    //-----------asignación del contenido que llevarán las etiquetas ya creadas----------
     TComentario.textContent = comentario;
-    Tfecha.textContent = fechaAct;
+    //Tfecha.textContent = fechaAct;
+    Tfecha.textContent = diaS + " " + diaN + " de " + mes + " a las " + hora +":" + minuto + ":" +segundo;
     Tusuario.textContent = nombre  + " " + apellido;
     TEliminar.textContent = "Eliminar";
     TEliminar.setAttribute('onclick', 'eliminar(this)');
@@ -83,10 +116,10 @@ formulario.addEventListener('submit', function(event){
 });
 
 
-function eliminar(btn){
-    let id = btn;
-    console.log(id.id);
-    btn.parentElement.parentElement.remove();
-    contador--;
-    spanContador.textContent = contador.toString();
+//--------FUNCIÓN PARA ELIMINAR COMENTARIOS----------
+function eliminar(btn){//toma como parámetros el botón actual, es decir, al que le hayan dado click
+    let btnA = btn;//lo guarda en una variable
+    btnA.parentElement.parentElement.remove();//el doble parentElement se asegura de eliminar los otros dos divs, el div padre y el div abuelo, siendo el div abuelo el que tiene todo lo del comentario
+    contador--;//decrementa el contador de los comentarios
+    spanContador.textContent = contador.toString();//actualiza el texto del span
 }
